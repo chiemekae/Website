f28c52f089b3696bad60b205e68ef79e04e52b2f
Ubuntu <chiemeka@ekwunazu.com>
2017-07-28 05:42:04 +0000

    Skills section

--- resources/views/index.php
+++ resources/views/index.php
@@ -97,6 +97,62 @@
             </div>
           </div>
         </div>
+        <!-- -->
+        <?php
+          $skills = DB::table('skills')->get();
+          $frontend = [];
+          $backend = [];
+          $other = [];
+          foreach ($skills as $skill) {
+            $skillname = $skill->name;
+            switch($skill->type) {
+              case "frontend":
+                array_push($frontend, $skillname);
+                break;
+              case "backend":
+                array_push($backend, $skillname);
+                break;
+              case "other":
+                array_push($other, $skillname);
+                break;
+            }
+          }
+          function skillsString($skillsarray){
+            $skillstring = "";
+            $i = 1;
+            foreach ($skillsarray as $skill){
+              $skillstring .= $skill;
+              if($i % 3 == 0){
+                $skillstring .= "<br>";
+              }
+              else {
+                $skillstring .= str_repeat('&nbsp;', 5);
+              }
+              $i++;
+            }
+            return $skillstring;
+          }
+        ?>
+        <div class="row">
+          <div class="col-sm-12">
+            <p class="title">My skills</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4">
+            <p class="tertiary-title">Frontend</p>
+            <p><?php echo skillsString($frontend); ?></p>
+          </div>
+          <div class="col-sm-4">
+            <p class="tertiary-title">Backend</p>
+            <p><?php echo skillsString($backend); ?></p>
+          </div>
+          <div class="col-sm-4">
+            <p class="tertiary-title">Other Skills</p>
+            <p><?php echo skillsString($other); ?></p>
+          </div>
+        </div>
+        <!-- -->
       </div>
     </div>
 
